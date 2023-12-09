1dde20ceb8051779c2a9fb49a138c03cc90b3d03
salaheddineazarg <salahazarag17@gmail.com>
2023-12-09 18:13:57 +0100

    AF-1<Fish>AF-7.AF-8.AF-9.AF-10.AF-11

--- src/main/java/com/spring/aftas/repositories/FishRepository.java
+++ src/main/java/com/spring/aftas/repositories/FishRepository.java
@@ -3,5 +3,9 @@ package com.spring.aftas.repositories;
 import com.spring.aftas.entities.Fish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FishRepository extends JpaRepository<Fish,String> {
+
+    Optional<Fish> findByName(String name);
 }
--- src/main/java/com/spring/aftas/repositories/LevelRepository.java
+++ src/main/java/com/spring/aftas/repositories/LevelRepository.java
@@ -3,5 +3,8 @@ package com.spring.aftas.repositories;
 import com.spring.aftas.entities.Level;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LevelRepository extends JpaRepository<Level,Long> {
+
 }
