84d36021761e6b5b8f8f19aefaa53282cd669aea
salaheddineazarg <salahazarag17@gmail.com>
2023-12-11 09:18:27 +0100

    AF-2<Level>AF-12.AF-13.AF-14.AF-15.AF-16

--- src/main/java/com/spring/aftas/services/Impl/LevelService.java
+++ src/main/java/com/spring/aftas/services/Impl/LevelService.java
@@ -1,12 +1,14 @@
 package com.spring.aftas.services.Impl;
 
 import com.spring.aftas.dto.level.LevelDTO;
+import com.spring.aftas.entities.Level;
 import com.spring.aftas.repositories.LevelRepository;
 import com.spring.aftas.services.interfaces.ILevelService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,26 +29,47 @@ public class LevelService  implements ILevelService {
 
     @Override
     public List<LevelDTO> getAllService() {
-        return null;
+
+
+        return Arrays.asList(modelMapper.map(this.levelRepository.findAll(),LevelDTO[].class));
     }
 
     @Override
     public Optional<LevelDTO> saveService(LevelDTO levelDTO) {
-        return Optional.empty();
+        Level level = modelMapper.map(levelDTO,Level.class);
+        level = this.levelRepository.save(level);
+
+        return Optional.ofNullable(modelMapper.map(level,LevelDTO.class));
     }
 
     @Override
     public boolean deleteService(Long Id) {
+        if (this.levelRepository.existsById(Id)){
+            this.levelRepository.deleteById(Id);
+            return true;
+        }
         return false;
     }
 
     @Override
     public Optional<LevelDTO> updateService(LevelDTO levelDTO, Long Id) {
+        if (this.levelRepository.existsById(Id)){
+            Level level = modelMapper.map(levelDTO,Level.class);
+            level = this.levelRepository.save(level);
+
+            return Optional.of(modelMapper.map(level,LevelDTO.class));
+        }
         return Optional.empty();
     }
 
     @Override
     public Optional<LevelDTO> findByIdService(Long Id) {
+        if (this.levelRepository.existsById(Id)){
+            Optional<Level> levelOptional = this.levelRepository.findById(Id);
+
+            return levelOptional.map(level -> modelMapper.map(level,LevelDTO.class));
+        }
         return Optional.empty();
+
     }
 }
