3c899ce0130cc21af33beac7f9306af24a0ea7a2
salaheddineazarg <salahazarag17@gmail.com>
2023-12-11 16:32:41 +0100

    AF-6<Competition>AF-34.AF-35.AF-36.AF-37.AF-38.AF-39

--- src/main/java/com/spring/aftas/services/interfaces/ICompetitionService.java
+++ src/main/java/com/spring/aftas/services/interfaces/ICompetitionService.java
@@ -2,12 +2,13 @@ package com.spring.aftas.services.interfaces;
 
 import com.spring.aftas.dto.competition.CompetitionDTO;
 import com.spring.aftas.dto.competition.CompetitionResponseDTO;
+import org.springframework.data.domain.Page;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 
 
 public interface ICompetitionService extends IData<CompetitionResponseDTO, CompetitionDTO,String> {
 
 
-    Pageable getCompetitionsByPagination(Pageable pageable);
+    Page<CompetitionResponseDTO> getCompetitionsByPagination(Pageable pageable);
 }
